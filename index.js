68850a42465ce96a7e05045e1b0704ac4f2e794c
Lauren Levaccare <laurenlevaccare@icloud.com>
2021-12-12 16:21:00 -0600

    analytics added

--- index.js
+++ index.js
@@ -2,13 +2,40 @@ const express = require ('express');
 const path = require('path');
 const fowlerMembers = require('./Members')
 const app = express();
+const fetch =require('node-fetch')
 
+const gtag= 'G-EHDX9LC20M'
 
+// const gtag= 'https://analytics.google.com/analytics/web/provision/?name=lauren+fowler#/p296575308/reports/reportinghub?params=_u..nav%3Dmaui%26_u..insightCards%3D%5B%7B%22question%22:%22track%22%7D%5D'
 
+app.get('/', async function (req, res) {
+    await fetch (
+        `https://www.google-analytics.com/collect?v=1&t=pageview&tid=${gtag}&cid=555&dp=tube`,
+        {
+            headers: {
+                'user-agent':
+                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+              },
+        }
+    )
+    return res.send ('do i work?')
+})
 
 app.get('/api/members',(req, res) => {
-res.json(fowlerMembers);
+    await fetch (
+        `https://www.google-analytics.com/collect?v=1&t=pageview&tid=${gtag}&cid=555&dp=%2Fhome`,
+        {
+            headers: {
+                'user-agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+
+            },
+        }
+    )
+   return res.json(fowlerMembers);
 })
+
+
 
 // app.get('/', (reg,res) => {
 //     res.sendFile(path.join(__dirname,'public','index.html'));
